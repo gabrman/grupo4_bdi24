8f4a5bb27f42cc9ce46e1917161e5ed418fa9adb
gabrman <89225578+gabrman@users.noreply.github.com>
2024-10-29 10:48:19 -0300

    Add files via upload
    Comparto los scripts que desarrollamos en la reunión del lunes 28/10. Incluyen la creación de usuarios y asignación de roles, pruebas de restricciones, y actualizaciones en las tablas de la base de datos.

---
+++ tema_manejo_de_permisos/create_rol_usuario.sql
@@ -0,0 +1,65 @@
+USE BASEDEDATOS_PLANTAS;
+use plantas_bd
+-- ----------------------------------------- 
+-- -------Usuario solo lectura--------------
+-- ----------------------------------------- 
+/* 1) Creamos un inicio de sesión */
+CREATE LOGIN Tomas WITH PASSWORD = 'def456';
+
+/* 2) Creamos un usuario de base de datos asociado al inicio de sesión */
+CREATE USER Tomas FOR LOGIN Tomas; 
+
+/* 3) Agregamos el usuario al rol db_datareader
+db_datareader es un rol predefinido en SQL Server otorga automáticamente permisos de lectura a todas las tablas,
+vistas y otras entidades de una base de datos.*/
+ALTER ROLE db_datareader ADD MEMBER Tomas; 
+
+-- ----------------------------------------- 
+-- -------Usuario Administrador-------------
+-- -----------------------------------------
+/* 1) Creamos un inicio de sesión */
+CREATE LOGIN Nicolas WITH PASSWORD = 'ghi789';
+
+/* 2) Creamos un usuario de base de datos asociado al inicio de sesión */
+CREATE USER Nicolas FOR LOGIN Nicolas; 
+
+/* 3) Agregamos el usuario al rol db_owner 
+db_owner es un rol que otorga a un usuario todos los permisos a nivel base de datos */
+ALTER ROLE db_owner ADD MEMBER Nicolas; 
+
+-- ----------------------------------------- 
+-- ------------Usuario Vendedor-------------
+-- -----------------------------------------
+/* 
+ Creación de un inicio de sesión para el usuario jime
+ Se crea un inicio de sesión de SQL Server para el usuario jime con la contraseña 'abc123'.
+ Este inicio de sesión será utilizado para autenticarse en la instancia de SQL Server. 
+*/
+CREATE LOGIN jime WITH PASSWORD = 'abc123';
+
+/*
+ Creación de un usuario de base de datos asociado al inicio de sesión.
+ Se crea un usuario de base de datos con el mismo nombre que el inicio de sesión.
+ Este usuario representa la identidad del usuario dentro de una base de datos específica.
+*/
+CREATE USER jime FOR LOGIN jime;
+
+/*
+ Creación de un rol personalizado para vendedores
+ Se crea un rol llamado 'Vendedor' para agrupar los permisos necesarios para un usuario con este perfil.
+*/
+CREATE ROLE Vendedor;
+
+/*
+ Asignación de permisos al rol Vendedor
+*/
+GRANT SELECT ON productos TO Vendedor;--Permite consultar los datos de la tabla.
+GRANT SELECT,INSERT,UPDATE ON pedido TO Vendedor;--Permite consultar, insertar y actualizar datos en la tabla.
+--GRANT SELECT ON detalles_pedido TO Vendedor;--Permite consultar los datos de la tabla.
+GRANT SELECT ON clientes TO Vendedor;--Permite consultar los datos de la tabla.
+
+/*
+ Asignación del usuario jime al rol Vendedor
+ El usuario jime se agrega al rol Vendedor, obteniendo todos los permisos asignados a este rol.
+*/
+EXEC sp_addrolemember 'Vendedor', 'jime';
---
+++ tema_manejo_de_permisos/prueba_restricciones_roles.sql
@@ -0,0 +1,64 @@
+use plantas_bd;
+-- -------------------------------------------------------------------------------
+--Prueba de restricciones de acceso para el usuario "Tomas" (rol de solo lectura)
+-- -------------------------------------------------------------------------------
+/* Probamos que el usuario "Tomas", quien posee el rol de solo lectura en la base de datos 
+ no pueda realizar otra operación. 
+*/
+/* Prueba 1: Consulta SELECT */
+SELECT * FROM clientes;
+--Puede consultar los datos de la tabla cliente y cualquier otra tabla de la base de datos.
+
+/* Prueba 2: Instrucción INSERT tabla 'clientes' */
+INSERT INTO clientes (id_cliente, nombre_cliente, apellido_cliente, email, telefono, direccion, dni)
+VALUES (1,'tomas', 'fernandez','tom@gmail.com','3794552255','callefalsa 123', 12314254);
+--Salida: Se denegó el permiso INSERT en el objeto 'clientes', base de datos 'BASEDEDATOS_PLANTAS', esquema 'dbo'.
+--Explicación: El usuario "Tomas" solo tiene permisos de lectura y no puede modificar los datos de la tabla al intentar insertar un nuevo registro.
+
+/* Prueba 3: Instrucción INSERT tabla 'productos' */
+INSERT INTO productos(id_producto, nombre_producto, precio, stock,descripcion, id_tipo_producto)
+VALUES (1, 'nombre_producto', 100, 5, 'descrip',1);
+--Salida: Se denegó el permiso INSERT en el objeto 'productos', base de datos 'BASEDEDATOS_PLANTAS', esquema 'dbo'.
+
+-- ----------------------------------------------------------------------------------
+-- Prueba de restricciones de acceso para el usuario "Nicolas" (Rol de Administrador)
+-- ----------------------------------------------------------------------------------
+/* Probamos las capacidades de inserción, consulta, actualización y eliminación
+ en las tablas 'clientes' y 'pedido' con el usuario "Nicolas", quien posee el rol
+ de administrador en la base de datos. 
+*/
+
+/* Prueba 1: Inserción en la Tabla 'clientes' */
+-- Esta operación añade un nuevo cliente a la tabla 'clientes'.
+-- Verificamos que el usuario "Nicolas" tiene permisos para crear registros.
+INSERT INTO clientes (nombre_cliente, apellido_cliente, email, telefono, direccion, dni)
+VALUES ('Tomas', 'Fernandez', 'tom@gmail.com', '3794552255', 'callefalsa 123', 12314254);
+-- Resultado esperado: (1 fila afectada) 
+
+/* Prueba 2: Instrucción INSERT tabla 'pedido' */
+-- Inserta un nuevo pedido en la tabla 'pedido', asociado al cliente recién creado.
+-- Verificamos que el usuario "Nicolas" tiene permisos de inserción.
+INSERT INTO pedido (fecha_pedido, monto_total, id_cliente)
+VALUES ('2024-10-28', 100, 1);
+-- Salida: (1 fila afectada) 
+
+/* Prueba 3: Consulta SELECT tabla 'pedido' */
+-- Consulta todos los registros de la tabla 'pedido'.
+-- Confirmamos que el usuario "Nicolas" tiene permisos de lectura.
+SELECT * FROM pedido;
+-- Salida: Visualización de todos los registros en 'pedido' (consulta exitosa).
+
+/* 4. Instrucción UPDATE tabla 'pedido' */
+-- Actualiza el monto total de un pedido en la tabla 'pedido'.
+-- Con esto mostramos que el usuario "Nicolas" puede modificar registros.
+UPDATE pedido
+SET monto_total = 200
+WHERE id_pedido = 1;
+-- Salida: (1 fila afectada) 
+
+/* 5. Instrucción DELETE tabla 'pedido' */
+-- Elimina un pedido específico en la tabla 'pedido'.
+-- Verificamos que el usuario "Nicolas" tiene permisos de eliminación.
+DELETE FROM pedido WHERE id_pedido = 1;
+-- Salida: (1 fila afectada) 
+
